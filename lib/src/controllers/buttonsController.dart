ece643abddb114956849f184d3e1db03722fc81a
junwoo <junwoolee@live.co.kr>
2023-11-24 13:52:11 +0900

    adding buttons added

---
+++ lib/src/controllers/buttonsController.dart
@@ -0,0 +1,24 @@
+import 'package:get/get.dart';
+import 'dart:math';
+
+class ButtonsController extends GetxController {
+// final ButtonsRepository repository;
+
+  static ButtonsController get to => Get.find();
+
+  ButtonsController();
+
+  // final _obj = ''.obs;
+  // set obj(value) => this._obj.value = value;
+  // get obj => this._obj.value;
+
+  final _buttonsCount = 1.obs;
+  set buttonsCount(value) => this._buttonsCount.value = value;
+  get buttonsCount => this._buttonsCount.value;
+
+  //가까운 제곱수 찾기 for 버튼 정렬
+  int closestSquare(int x) {
+    double sqrtX = sqrt(x);
+    return sqrtX.ceil().toInt();
+  }
+}
--- lib/src/pages/homepage/homepage.dart
+++ lib/src/pages/homepage/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:snapy/src/controllers/buttonsController.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,6 +8,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    Get.put(ButtonsController());
 
     return Scaffold(
       backgroundColor: theme.colorScheme.background,
@@ -16,34 +19,104 @@ class HomePage extends StatelessWidget {
             SizedBox(
               width: 300,
               height: 300,
-              child: GridView.builder(
-                shrinkWrap: true, // GridView의 크기를 자식들의 크기에 맞춥니다.
-                physics: NeverScrollableScrollPhysics(), // 스크롤을 비활성화합니다.
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 10, // 가로 간격을 조정합니다.
-                  mainAxisSpacing: 10, // 세로 간격을 조정합니다.
-                  childAspectRatio: 1, // 자식들의 가로세로 비율을 1:1로 설정합니다.
-                ),
-                itemCount: 4, // 4개의 아이템을 생성합니다.
-                itemBuilder: (context, index) {
-                  const double br = 20;
-                  return InkWell(
-                    borderRadius: BorderRadius.circular(br),
-                    onTap: () {},
-                    child: Ink(
-                      height: 50, // 동그라미의 높이를 설정합니다.
-                      width: 50, // 동그라미의 너비를 설정합니다.
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(br),
-                        color: theme.colorScheme.primary, // 배경색을 설정합니다.
-                      ),
+              child: Obx(() {
+                return AnimatedSwitcher(
+                  duration: Duration(milliseconds: 500),
+                  switchInCurve: Curves.easeOut,
+                  switchOutCurve: Curves.easeIn,
+                  transitionBuilder: (Widget child, Animation<double> anim) {
+                    return RotationTransition(
+                      turns: anim,
+                      child: FadeTransition(child: child, opacity: anim),
+                    );
+                  },
+                  child: GridView.builder(
+                    key: ValueKey<int>(ButtonsController.to.buttonsCount),
+                    shrinkWrap: true, // GridView의 크기를 자식들의 크기에 맞춥니다.
+                    physics: NeverScrollableScrollPhysics(), // 스크롤을 비활성화합니다.
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: ButtonsController.to
+                          .closestSquare(ButtonsController.to.buttonsCount),
+                      crossAxisSpacing: 10, // 가로 간격을 조정합니다.
+                      mainAxisSpacing: 10, // 세로 간격을 조정합니다.
+                      childAspectRatio: 1, // 자식들의 가로세로 비율을 1:1로 설정합니다.
                     ),
-                  );
-                },
-              ),
+                    itemCount:
+                        ButtonsController.to.buttonsCount, // 4개의 아이템을 생성합니다.
+                    itemBuilder: (context, index) {
+                      const double br = 20;
+                      return AnimatedGridItem(
+                        index: index,
+                        onTap: () {
+                          ButtonsController.to.buttonsCount += 1;
+                        },
+                      );
+                    },
+                  ),
+                );
+              }),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class AnimatedGridItem extends StatefulWidget {
+  final int? index; // 아이템의 인덱스
+  final Function() onTap;
+
+  AnimatedGridItem({Key? key, this.index, required this.onTap})
+      : super(key: key);
+
+  @override
+  _AnimatedGridItemState createState() => _AnimatedGridItemState();
+}
+
+class _AnimatedGridItemState extends State<AnimatedGridItem>
+    with SingleTickerProviderStateMixin {
+  AnimationController? _controller;
+  Animation<double>? _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    _animation = Tween<double>(begin: 0, end: 1).animate(_controller!)
+      ..addListener(() {
+        setState(() {});
+      });
+
+    _controller!.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller!.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return ScaleTransition(
+      scale: _animation!,
+      child: InkWell(
+        // 여기서 아이템의 UI를 구성합니다.
+        onTap: widget.onTap,
+        child: Ink(
+          decoration: BoxDecoration(
+            color: theme.colorScheme.primary,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Center(
+            child: Text('Item ${widget.index}'),
+          ),
         ),
       ),
     );
